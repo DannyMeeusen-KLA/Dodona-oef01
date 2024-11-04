2f3a1cd32c5fdad76b590d687b22dde12ba67403
DannyMeeusen-KLA <danny.meeusen@kla.be>
2024-11-04 22:01:56 +0100

    lijsten01, v02

--- exercises/lijsten01/solution/solution.py
+++ exercises/lijsten01/solution/solution.py
@@ -1,8 +1,8 @@
 zin = input('Geef 3 getallen, gescheiden door spaties: ')
 strlijst=zin.split(' ')
 intlijst=[]
-intlijst.append(int(lijst[0]))
-intlijst.append(int(lijst[1]))
-intlijst.append(int(lijst[2]))
+intlijst.append(int(strlijst[0]))
+intlijst.append(int(strlijst[1]))
+intlijst.append(int(strlijst[2]))
 
 print(intlijst)
