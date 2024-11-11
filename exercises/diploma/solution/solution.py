9576a9dd893f601e2d506b6cf086f6e997b06b4f
DannyMeeusen-KLA <danny.meeusen@kla.be>
2024-11-11 22:30:40 +0100

    Diploma, v03

--- exercises/diploma/solution/solution.py
+++ exercises/diploma/solution/solution.py
@@ -2,8 +2,8 @@ leerling = input('Geef de naam van de leerling: ')
 percent = int(input('Geef het percentage op het eindexamen: '))
 
 if percent >= 80:
-    print (leerling, 'haalde een diploma met grote onderscheiding.')
+    print (leerling, 'behaalde een diploma met grote onderscheiding.')
 elif percent >= 70:
-    print (leerling, 'haalde een diploma met onderscheiding.')
+    print (leerling, 'behaalde een diploma met onderscheiding.')
 else:
-    print (leerling, 'haalde een diploma.')
+    print (leerling, 'behaalde een diploma.')
