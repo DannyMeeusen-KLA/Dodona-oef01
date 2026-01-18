077ae0bb7eaa59ac194497801dbd29bfe803badd
DannyMeeusen-KLA <danny.meeusen@kla.be>
2026-01-18 23:34:52 +0100

    v02

--- exercises/lijsten08/solution/solution.py
+++ exercises/lijsten08/solution/solution.py
@@ -1,7 +1,7 @@
 onder = int(input('Geef de ondergrens: '))
 boven = int(input('Geef de bovengrens: '))
 
-lijst = [*range(onder, boven, 1)]
+lijst = [*range(onder, boven+1, 1)]
 
 if 24 in lijst:
     lijst.remove(24)
@@ -11,4 +11,4 @@ print (aantal)
 if aantal < 10:
     print ('Te weinig elementen!')
 else:
-    print (lijst[9])
+    print ('Het tiende element is', lijst[9])
