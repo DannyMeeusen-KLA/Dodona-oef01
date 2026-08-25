bbd12847fcd3ac0a5208fa296f88f53288659240
DannyMeeusen-KLA <danny.meeusen@kla.be>
2026-08-25 20:27:35 +0200

    SC03, v01

---
+++ exercises/syntaxcorr03/description/description.nl.md
@@ -0,0 +1,6 @@
+Het goede nieuws is dat je van ons de oplossing van het programma cadeau krijgt.  
+Het slechte nieuws is dat er behoorlijk wat syntaxfouten in zitten. Bij een syntaxfout is er gezondigd tegen de regels van Python die je in eerdere oefeningen leerde.  
+  
+In elke paragraaf staat telkens eerst wat de bedoeling is en dan de voorgestelde oplossing. Aan jou om die laatste te corrigeren.  
+Let op: zowel de input()- als de print()-instructies kunnen fouten bevatten.  
+    
---
+++ exercises/syntaxcorr03/solution/solution.py
@@ -0,0 +1,13 @@
+# Opgave 1: Vraag hoeveel tickets je voor de schoolvoorstelling gekocht hebt en hoeveel ze kosten.
+# Druk in een volzin af hoeveel je moet betalen.  
+tickets = int(input("Hoeveel tickets kocht je?"))
+prijs = int(input("Hoeveel kost één ticket?"))
+print ('Je betaalde', tickets*prijs, 'euro.')
+
+# Opgave 2: Vraag iemands naam en daarna wat zijn lievelingskleur is  
+# Druk daarna alles af in de volzin    <naam> houdt van <kleur>.  
+naam = input('Geef je naam: ')  
+kleur = input('Wat is je lievelingskleur? ')  
+zin = naam + " houdt van " + kleur + "."
+print (zin)  
+ 
